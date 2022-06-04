aa6a100d33db9959a28dabeeced070cb94fff2bb
Nanthagopal98 <nanthagopal.sj@gmail.com>
2022-06-04 14:49:26 +0530

    [Nantha] - Email Added

--- Regex_User_Registration/Regex_User_Registration/Program.cs
+++ Regex_User_Registration/Regex_User_Registration/Program.cs
@@ -1,6 +1,7 @@
 ﻿Regex_User_Registration.RegularExpression regex = new Regex_User_Registration.RegularExpression();
 Console.WriteLine("1 - Enter First Name");
 Console.WriteLine("2 - Enter Last Name");
+Console.WriteLine("3 - Enter Enail-id");
 int select =Convert.ToInt32(Console.ReadLine());
 switch (select)
 {
@@ -13,6 +14,11 @@ switch (select)
         Console.WriteLine("Enter Last Name");
         string lName = Console.ReadLine();
         regex.name(lName);
+        break;
+    case 3:
+        Console.WriteLine("Enter Enail-id");
+        string id = Console.ReadLine();
+        regex.email(id);
         break;
 
 }
--- Regex_User_Registration/Regex_User_Registration/Regex.cs
+++ Regex_User_Registration/Regex_User_Registration/Regex.cs
@@ -21,5 +21,18 @@ namespace Regex_User_Registration
             Console.WriteLine(name + " is a invalid Name");
             return;
         }
+        public void email(string id)
+        {
+            const string mailId = "^[a-zA-z0-9]+[.]{0,1}[a-zA-z0-9]+([@]?)+[a-z]{1,15}[.]+(com|co)+((.in)*)$";
+            if(Regex.IsMatch(id, mailId))
+            {
+                Console.WriteLine(id + " is a valid Email");
+                return;
+            }
+            Console.WriteLine(id + " is a invalid Email");
+            return;
+
+        }
+        
     }
 }

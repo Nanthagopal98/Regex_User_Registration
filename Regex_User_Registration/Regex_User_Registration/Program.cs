ef7b291eab0eb382a92035cfdd41b2daa05bf7a6
Nanthagopal98 <nanthagopal.sj@gmail.com>
2022-06-04 12:15:08 +0530

    [Nantha] - First Name Added

---
+++ Regex_User_Registration/Regex_User_Registration/Program.cs
@@ -0,0 +1,13 @@
+﻿Regex_User_Registration.RegularExpression regex = new Regex_User_Registration.RegularExpression();
+Console.WriteLine("1 - Enter First Name");
+Console.WriteLine("2 - Enter Last Name");
+int select =Convert.ToInt32(Console.ReadLine());
+switch (select)
+{
+    case 1:
+        Console.WriteLine("Enter First Name");
+        string fName = Console.ReadLine();
+        regex.name(fName);
+        break;
+    
+}
---
+++ Regex_User_Registration/Regex_User_Registration/Regex.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace Regex_User_Registration
+{
+    internal class RegularExpression
+    {
+        public void name(string name)
+        {
+            const string NAME = "^[A-Z]{1}[a-z]{2,}$";
+                      
+            if(Regex.IsMatch(name, NAME))
+            {
+                Console.WriteLine(name + " is a valid Name");
+                return;
+            }
+            Console.WriteLine(name + " is a invalid Name");
+            return;
+        }
+    }
+}

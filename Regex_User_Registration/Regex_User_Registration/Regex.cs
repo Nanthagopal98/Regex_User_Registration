f54344e5796593476c56abd08b7a8e65c236bc3b
Nanthagopal98 <nanthagopal.sj@gmail.com>
2022-06-13 12:46:56 +0530

    [Nantha] - Lambda Function Added

--- Regex_User_Registration/Regex_User_Registration/Regex.cs
+++ Regex_User_Registration/Regex_User_Registration/Regex.cs
@@ -12,9 +12,10 @@ namespace Regex_User_Registration
         public string FirstName(string name)
         {
             const string NAME = "^[A-Z]{1}[a-z]{2,}$";
+            Func<string, bool> result = (name) => (Regex.IsMatch(name, NAME));
             try
             {
-                if (Regex.IsMatch(name, NAME))
+                if (result(name))
                 {
                     Console.WriteLine(name + " is a valid Name");
                     return name;
@@ -33,9 +34,10 @@ namespace Regex_User_Registration
         public string LastName(string name)
         {
             const string NAME = "^[A-Z]{1}[a-z]{2,}$";
+            Func<string, bool> result = (name) => (Regex.IsMatch(name, NAME));
             try
             {
-                if (Regex.IsMatch(name, NAME))
+                if (result(name))
                 {
                     Console.WriteLine(name + " is a valid Name");
                     return name;
@@ -54,7 +56,8 @@ namespace Regex_User_Registration
         public string email(string id)
         {
             const string mailId = "^[a-zA-z0-9]+([.+-]?[a-zA-z0-9]+)@[a-z0-9]+[.]?(com|co|net)+((.in|.au|.us)*)$";
-            if(Regex.IsMatch(id, mailId))
+            Func<string, bool> result = (id) => (Regex.IsMatch(id, mailId));
+            if (result(id))
             {
                 Console.WriteLine(id + " is a valid Email");
                 return "Valid";
@@ -70,7 +73,8 @@ namespace Regex_User_Registration
         public bool ContactNumber(string number)
         {
             const string numberRegex = "^(91 )[6-9]{1}[0-9]{9}$";
-            if (Regex.IsMatch(number, numberRegex))
+            Func<string, bool> result = (number) => (Regex.IsMatch(number, numberRegex));
+            if (result(number))
             {
                 Console.WriteLine(number + " is a valid Number");
                 return true;
@@ -84,7 +88,8 @@ namespace Regex_User_Registration
         public bool Password(string password)
         {
             const string passwordRegex = "^(?=[A-za-z0-9,]*[!#@$%^&][a-zA-Z0-9]*$)[]a-zA-Z0-9,!#@$%^&]+.{8,}";
-            if (Regex.IsMatch(password, passwordRegex))
+            Func<string, bool> result = (password) => (Regex.IsMatch(password, passwordRegex));
+            if (result(password))
             {
                 Console.WriteLine(password + " is a valid Password");
                 return true;
